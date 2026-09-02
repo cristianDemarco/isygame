ea62c2cf3708e2981eaab7e15dd57e6ff97b1fe9
cristianDemarco <demarcocristian07@gmail.com>
2026-09-02 20:10:35 +0200

    feat:dockerization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_BACKEND_URL || 'http://backend:8080',
         changeOrigin: true,
       }
     }
